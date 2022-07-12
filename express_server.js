11ac3243ac809328d50ebab8677c8b3014eed7e9
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-12 20:49:44 +0000

    Added post route that updates the value of our stored long URL and redirects to /urls

--- express_server.js
+++ express_server.js
@@ -25,38 +25,10 @@ const urlDatabase = {
 
 
 // ROUTING
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-// Displays our urls in the urlDatabase by using urls_index template
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-// Allows us to render a new website URL and displays it with the urls_new template
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-// Displays short URL and long URL
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
-  res.render("urls_show", templateVars);
-});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
-// Generates short URL that is added to database and redirected to urls/:id
-app.post("/urls", (req, res) => {
-  let newId = generateRandomString();
-  urlDatabase[newId] = req.body.longURL;
-  res.redirect(`urls/${newId}`); 
-});
-
 
 app.get("/u/:id", (req, res) => {
   const longURL = urlDatabase[req.params.id];
@@ -67,7 +39,42 @@ app.get("/u/:id", (req, res) => {
   }
 });
 
+// Allows us to render a new website URL and displays it with the urls_new template
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+// delete url
 app.post('/urls/:id/delete', (req, res) => {
   delete urlDatabase[req.params.id]
   res.redirect('/urls')
 })
+
+// edit request
+app.post('/urls/:id/', (req, res) => {
+  urlDatabase[req.params.id] = req.body.longURL
+  res.redirect('/urls')
+})
+
+// Generates short URL that is added to database and redirected to urls/:id
+app.post("/urls", (req, res) => {
+  let newId = generateRandomString();
+  urlDatabase[newId] = req.body.longURL;
+  res.redirect(`urls/${newId}`); 
+});
+
+// Displays short URL and long URL
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
+// Displays our urls in the urlDatabase by using urls_index template
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
