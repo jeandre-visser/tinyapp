ee264200d2b6ad5ada0b560d9ccb998f868f4073
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-12 03:42:52 +0000

    Added comments

--- express_server.js
+++ express_server.js
@@ -24,19 +24,23 @@ const urlDatabase = {
 };
 
 
+// ROUTING
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// Displays our urls in the urlDatabase by using urls_index template
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+// Allows us to render a new website URL and displays it with the urls_new template
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+// Displays short URL and long URL
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
@@ -46,7 +50,7 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-
+// Generates short URL that is added to database and redirected to urls/:id
 app.post("/urls", (req, res) => {
   let newId = generateRandomString();
   urlDatabase[newId] = req.body.longURL;
