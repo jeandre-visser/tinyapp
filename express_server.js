66366966cdf02e0a9d31e5b6066f7d2c3be6139b
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-12 03:00:49 +0000

    Updated to allow for the id longURL key value pair to be saved to the urlDatabase when reveiving a POST request to /urls

--- express_server.js
+++ express_server.js
@@ -23,6 +23,7 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+// GET
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -36,11 +37,6 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
-
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
@@ -50,10 +46,11 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+// POST
+app.post("/urls", (req, res) => {
+  let newId = generateRandomString();
+  urlDatabase[newId] = req.body.longURL;
+  res.redirect(`urls/${newId}`); // Respond with 'Ok' (we will replace this)
 });
-
-
 
 
