1c7630ec5eed24564d33e0cb96d4927871a32509
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-14 20:36:53 +0000

    Added getUserByEMail helper function

---
+++ helpers.js
@@ -0,0 +1,11 @@
+// find user with email in database
+const getUserByEmail = (email, database) => {
+  for (const user in database) {
+    if (database[user].email === email) {
+      return database[user];
+    }
+  }
+  return undefined;
+}
+
+module.exports = { getUserByEmail };
