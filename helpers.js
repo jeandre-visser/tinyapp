777e300cf142657efbf5ac5cdf3f029e3866d0ea
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-15 00:15:20 +0000

    Added two more help functions to make process simpler in express_server

--- helpers.js
+++ helpers.js
@@ -28,4 +28,24 @@ const urlsForUser = (id, database) => {
   return userUrls;
 }
 
-module.exports = { getUserByEmail, generateRandomString, urlsForUser };
+// Validates if email exists yet
+const validateEmail = (email, database) => {
+  for (const key in database) {
+    if (database[key].email === email) {
+      return true;
+    }
+  }
+  return false;
+};
+
+// Input an id and extract email
+const extractEmail = (id, database) => {
+  for (const key in database) {
+    if (database[key].id === id) {
+      return database[key].email
+    }
+  }
+}
+
+
+module.exports = { getUserByEmail, generateRandomString, urlsForUser, extractEmail, validateEmail };
