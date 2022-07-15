c94574319a5c596bb77c63d4755c46351018513a
Jeandre Visser <jeandrev1414@gmail.com>
2022-07-15 16:48:45 +0000

    Eslint fixes

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -4,25 +4,25 @@ const { getUserByEmail } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
-    id: "userRandomID", 
-    email: "user@example.com", 
+    id: "userRandomID",
+    email: "user@example.com",
     password: "purple-monkey-dinosaur"
   },
   "user2RandomID": {
-    id: "user2RandomID", 
-    email: "user2@example.com", 
+    id: "user2RandomID",
+    email: "user2@example.com",
     password: "dishwasher-funk"
   }
 };// getUserByEmailTest
 describe('#getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("user@example.com", testUsers)
+    const user = getUserByEmail("user@example.com", testUsers);
     const expectedUserID = "userRandomID";
-    assert.equal(user, testUsers.userRandomID)
+    assert.equal(user, testUsers.userRandomID);
   });
 
   it('should return undefined when looking for a non-existent email', function() {
-    const user = getUserByEmail("non-existent@example.com", testUsers)
-    assert.equal(user, undefined)
+    const user = getUserByEmail("non-existent@example.com", testUsers);
+    assert.equal(user, undefined);
   });
 });
